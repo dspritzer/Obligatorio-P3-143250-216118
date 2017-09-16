d569851fc39c57041a5ba5905cd1d222b056bf7c
Periegnis <leoper.14@gmail.com>
2017-09-16 15:59:21 -0300

    Add files via upload

---
+++ ServiciosWCF/ServiceWCFProveedores.svc.cs
@@ -0,0 +1,24 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiciosWCF
+{
+   
+    public class ServiceWCFProveedores : IServiceWCFProveedores
+    {
+        
+
+        public DataSet ObtenerTodos()
+        {
+            DataSet lista = Proveedor.ListarProveedores();
+            return lista;
+        }
+    }
+}
